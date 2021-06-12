5f8711621575cc974fa5fd2506395eb059b6c82d
PYlamp <73482519+PYlamp@users.noreply.github.com>
2021-06-12 15:18:03 -0500

    Update script.py
    fixed big time bugs eh

--- script.py
+++ script.py
@@ -1,9 +1,13 @@
 import PIL
 from PIL import Image
+    
 
 ASCII_CHARS = ["@", "#","S","%","$","%","?","*","+",";",":",",","."]
 
+
+
 def resize_image(image, new_width=100):
+    #size = 128, 128
     width, height = image.size
     ratio = height / width
     new_height = int(new_width * ratio)
@@ -17,18 +21,18 @@ def grayify(image):
 
 def pixel_to_ascii(image):
   pixels = image.getdata()
-  charaters = " ".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+  charaters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
   return(charaters)
 
 
 def main(new_width=100):
   path = input('Enter a valid pathname to an image:\n')
   try:
-    image = PIL.Image.open(path)
+    image = Image.open(path)
   except:
       print(path, "is no valid.")
 
-  new_image_data = pixel_to_ascii(grayify(resize_image(Image)))
+  new_image_data = pixel_to_ascii(grayify(resize_image(image)))
 
   pixel_count = len(new_image_data)
   ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
@@ -36,5 +40,6 @@ def main(new_width=100):
   print(ascii_image)
   with open("ascii_image.txt", "w") as f:
     f.write(ascii_image)
+
 
 main()
